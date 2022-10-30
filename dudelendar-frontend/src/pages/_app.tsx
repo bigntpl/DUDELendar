6ab1a355dc604c4c45a4c9cb0bd5db2d58a8c0b9
bigntpl <nattapon.ler@hotmail.com>
2022-10-30 19:05:05 +0700

    wip: add navbar

--- dudelendar-frontend/src/pages/_app.tsx
+++ dudelendar-frontend/src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { CssBaseline } from '@mui/material';
 import type { AppProps } from 'next/app';
 import { GlobalStyle } from '../styles/global';
+import { Navbar } from '../components/Navbar';
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+        <Navbar />
         <CssBaseline />
         <GlobalStyle />
         <Component {...pageProps} />
