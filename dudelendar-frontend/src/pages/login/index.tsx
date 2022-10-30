48e54864ffd966d0c623802011d33c5b54ec296e
bigntpl <nattapon.ler@hotmail.com>
2022-10-30 21:00:04 +0700

    wip: working on login component

--- dudelendar-frontend/src/pages/login/index.tsx
+++ dudelendar-frontend/src/pages/login/index.tsx
@@ -1,15 +1,19 @@
-import Navbar from "../../components/Navbar";
-// import { Input } from "@chakra-ui/react";
+import Navbar from '../../components/Navbar'
+import { Input, Button } from '@chakra-ui/react'
 
 const login = () => {
   return (
     <>
       <div className="container mx-auto my-60 w-48">
         <div className="my-auto mx-auto">
-          <div className="mx-auto">
-            {/* <form>
-            </form> */}
-            Hello from Login page
+          <div className="mx-auto bg-green-50 w-60 p-3 border-collapse rounded-lg">
+            <form>
+              <Input color="black" type="text" placeholder="Username" />
+              <Input color="black" type="text" placeholder="Password" />
+              <Button type="submit" className="w-full" colorScheme="green">
+                Login
+              </Button>
+            </form>
           </div>
         </div>
       </div>
