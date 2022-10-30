16060b8b5828f54ee3832ee6c5618fcf8a24b338
bigntpl <nattapon.ler@hotmail.com>
2022-10-30 18:32:08 +0700

    init: add tailwindcss

--- dudelendar-frontend/src/pages/_app.tsx
+++ dudelendar-frontend/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { CssBaseline } from '@mui/material';
 import type { AppProps } from 'next/app';
 import { GlobalStyle } from '../styles/global';
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
---
+++ dudelendar-frontend/src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+const login = () => {
+  return (
+    <>
+      <div className="text-2xl container mx-auto">
+        <div className="mx-auto">Hello, this is from login page</div>
+      </div>
+    </>
+  )
+}
+
+export default login
