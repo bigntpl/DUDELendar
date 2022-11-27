22f24eb3f24be8a5dcb07d6c827db1df16c6a4d1
planwis <witch2544@hotmail.co.th>
2022-11-27 16:59:26 +0700

    remove unused variable

--- dudelendar-backend/services/db.js
+++ dudelendar-backend/services/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 const config = require('../config');
 
-async function query(sql, params) {
+async function query(sql) {
   const connection = await mysql.createConnection(config.db);
-  const [results, ] = await connection.execute(sql, params);
+  const [results, ] = await connection.execute(sql);
 
   return results;
 }
